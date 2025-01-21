ec1a13509450c15bc924c7f0e5b54c1383767f53
cfabi576 <alberto.catboy2@gmail.com>
2025-01-21 15:22:25 -0500

    Update demoMod.js

--- js/Demo/demoMod.js
+++ js/Demo/demoMod.js
@@ -44,12 +44,14 @@ function getPointGen() {
 	let gain = new Decimal(1)
 	if (hasUpgrade("c", 12)) gain = gain.times(upgradeEffect("c", 12))
 	return gain
-
-	upgrades: { 11;
-		 {
-        description: "Double your point gain.",
-        cost: new Decimal(1),
-        if (hasUpgrade('p', 11)) gain = gain.times(2)
+	upgrades: {
+		11: {
+			description: "Blah",
+			cost: new Decimal(100),
+			etc
+		},
+		etc
+	}
 }
 
 // You can add non-layer related variables that should to into "player" and be saved here, along with default values
