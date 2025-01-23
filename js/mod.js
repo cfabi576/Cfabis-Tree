419bd38214145501ba2efd6ec8e6e279e0f431b1
cfabi576 <alberto.catboy2@gmail.com>
2025-01-23 14:35:03 -0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -71,7 +71,7 @@ var displayThings = [
 
 // Determines when the game "ends"
 function isEndgame() {
-	return player.points.gte(new Decimal("1e33"))
+	return player.points.gte(new Decimal("1e63"))
 }
 
 
