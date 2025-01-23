c6422ca93d82447d3e426b7c850640fd2fb0a53c
cfabi576 <alberto.catboy2@gmail.com>
2025-01-23 13:54:00 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -126,7 +126,7 @@ addLayer("p", {
                                                 // Also the amount required to unlock the layer.
     
         type: "normal",                         // Determines the formula used for calculating prestige currency.
-        exponent: 0.5,                          // "normal" prestige gain is (currency^exponent).
+        exponent: 0.2,                          // "normal" prestige gain is (currency^exponent).
     
         gainMult() {                            // Returns your multiplier to your gain of the prestige resource.
             return new Decimal(1)               // Factor in any bonuses multiplying gain here.
