ebb219d06b988dd4b6e0a4fa3831d5fac6f2509e
cfabi576 <alberto.catboy2@gmail.com>
2025-01-24 11:26:29 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -375,9 +375,9 @@ addLayer("p", {
             branches: ["l"],
         
             baseResource: "points",             // The name of the resource your prestige gain is based on.
-            baseAmount() { return player.points },  // A function to return the current amount of baseResource.
+            baseAmount() { return player.a },  // A function to return the current amount of baseResource.
         
-            requires: new Decimal(1e100),              // The amount of the base needed to  gain 1 of the prestige currency.
+            requires: new Decimal(1e1000),              // The amount of the base needed to  gain 1 of the prestige currency.
                                                     // Also the amount required to unlock the layer.
         
             type: "normal",                         // Determines the formula used for calculating prestige currency.
